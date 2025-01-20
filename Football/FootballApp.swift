72158f81081182fa4535ee66049a94a4069bf9b8
Farasatullah <50820479+Farasatullah@users.noreply.github.com>
2025-01-20 13:51:43 +0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Farasat's_MacBook_Pro on 20/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
