faa4a63a139c7027f00a78d0f836f9c78d054616
Farasatullah <50820479+Farasatullah@users.noreply.github.com>
2025-02-06 18:10:06 +0500

    updated assets are

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+                .padding(.bottom, 20)
+            HStack{
+        
+                List{
+                    HStack{
+                        Image("football_pencil")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 100)
+                            .padding(.leading,10)
+                        Text("What is Football to You?")
+                        Button("Play", role: .none) {
+                            print("Start Playing")
+                        }
+                        .frame(maxWidth: 100)
+                    }
+
+                    
+                    Image("football_icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 100)
+                        .padding(.trailing,10)
+                    Image("football_kicks")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: 100)
+                        .padding(.trailing,10)
+                }
+
+
+            }
         }
-        .padding()
     }
 }
 
